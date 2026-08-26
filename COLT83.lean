56386b8be6f9bf198426230c67fb892b9a06be47
Remy Degenne <remydegenne@gmail.com>
2026-08-26 10:37:03 +0200

    mk_all

--- COLT83.lean
+++ COLT83.lean
@@ -2,14 +2,14 @@ module  -- shake: keep-all --deprecated_module: ignore
 
 public import COLT83.LeanMachineLearning.IdentAlg
 public import COLT83.LeanMachineLearning.LinearBandit
-public import COLT83.Mathlib.GaussianWidth
-public import COLT83.Mathlib.MeasurableSigma
-public import COLT83.MXJ2026.StructuredSets
-public import COLT83.MXJ2026.Width
 public import COLT83.MXJ2026.LogGains
 public import COLT83.MXJ2026.LowerAdaptive
 public import COLT83.MXJ2026.LowerNonadaptive
 public import COLT83.MXJ2026.NormEstimation
 public import COLT83.MXJ2026.Separation
+public import COLT83.MXJ2026.StructuredSets
 public import COLT83.MXJ2026.Upper
+public import COLT83.MXJ2026.Width
 public import COLT83.MXJ2026.WidthBounds
+public import COLT83.Mathlib.GaussianWidth
+public import COLT83.Mathlib.MeasurableSigma
